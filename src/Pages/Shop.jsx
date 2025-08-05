22faf1dafdeac055e140fd48db0b84110ea841ea
imakash-24 <imakash2409@gmail.com>
2025-08-05 23:24:30 +0530

    fix

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
-import { Popular } from '../Components/popular/Popular'
+import { Popular } from '../Components/Popular/Popular'
 import { Offer } from '../Components/Offer/Offer'
 import { NewCollections } from '../Components/NewCollections/NewCollections'
 import { NewsLetter } from '../Components/NewsLetter/NewsLetter'
